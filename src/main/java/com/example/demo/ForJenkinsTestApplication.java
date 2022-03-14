d1bcef1ff3fcf25421d68cc9c47f55db64c83205
seongyun.lim <seongyun.lim@e4net.net>
2022-03-14 19:51:30 +0900

    init

---
+++ src/main/java/com/example/demo/ForJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForJenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForJenkinsTestApplication.class, args);
+	}
+
+}
